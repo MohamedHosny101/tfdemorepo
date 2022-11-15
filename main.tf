c2fde56940ecd109296b3c6cf9f271a268876467
MohamedHosny101 <61685425+MohamedHosny101@users.noreply.github.com>
2022-11-15 11:47:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "9707751e-bb10-42ab-a3b2-cd05593218ae"
-  tenant_id = "1978815e-5a36-47ee-b0de-fbab3ddc6e68"
-  client_id = "7bb76627-0920-46b7-87a7-7603168000de"
-  client_secret = "6q68Q~R2icDyJtOzSMrsPwy.Ib6X3JhYP9TvPcZl"
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
+  client_secret = ""
   features {}  
 }
 
